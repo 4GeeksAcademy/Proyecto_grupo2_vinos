76e1d51e59a0d9cdca4ca062c5d1364119efb96c
Pauline-73 <pauline.charvin@gmail.com>
2025-02-28 16:41:50 +0100

    tintos hasta 16900

--- conversor/Conversor2.0.py
+++ conversor/Conversor2.0.py
@@ -26,16 +26,16 @@ options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) Apple
 driver = webdriver.Chrome(options=options)
 
 # Read URLs from a .txt file
-with open('/Users/josetudela/Projects/vinos_grupo2/Proyecto_grupo2_vinos/lista_tintos_unicos.txt', 'r') as file:
+with open('\Users\Pauline\Downloads\Proyecto_grupo2_vinos\conversor\lista_tintos_unicos.txt', 'r') as file:
     urls_list = [line.strip() for line in file.readlines()]
 
 # List to store new URLs
 new_urls = []
 batch_size = 100
-batch_number = 41
+batch_number = 150
 
 # Process each URL in the list
-for original_url in urls_list[4100:]:
+for original_url in urls_list[1500:]:
     print(f"Procesando URL: {original_url}")  # Show progress
 
     try:
