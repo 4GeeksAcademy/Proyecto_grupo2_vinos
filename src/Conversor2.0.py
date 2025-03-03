4ca64900995b1e7f50a72c3979b90b9a65c33d99
Chematudela <chematudela@gmail.com>
2025-03-03 22:07:28 +0100

    3mar

--- src/Conversor2.0.py
+++ src/Conversor2.0.py
@@ -7,7 +7,7 @@ import time
 
 # Function to save URLs to a file
 def save_urls_to_file(urls, batch_number):
-    filename = f'def_tintos{batch_number}.txt'
+    filename = f'Vino Blanco 10200 {batch_number}.txt'
     with open(filename, 'w') as file:
         for url in urls:
             file.write(url + '\n')
@@ -26,16 +26,16 @@ options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) Apple
 driver = webdriver.Chrome(options=options)
 
 # Read URLs from a .txt file
-with open('/Users/josetudela/Projects/vinos_grupo2/Proyecto_grupo2_vinos/lista_tintos_unicos.txt', 'r') as file:
+with open('/Users/josetudela/Projects/vinos_grupo2/Proyecto_grupo2_vinos/vinoblanco10200.txt', 'r') as file:
     urls_list = [line.strip() for line in file.readlines()]
 
 # List to store new URLs
 new_urls = []
 batch_size = 100
-batch_number = 131
+batch_number = 6
 
 # Process each URL in the list
-for original_url in urls_list[13100:]:
+for original_url in urls_list[600:]:
     print(f"Procesando URL: {original_url}")  # Show progress
 
     try:
